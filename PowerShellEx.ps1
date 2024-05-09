d5564aef63a3cfe7e08a89b1133b411cc28eba9f
galivachka <@muhammadusmonov182@gmail.com>
2024-05-09 18:51:38 +0500

    Update PowerShellEx.ps1

--- PowerShellEx.ps1
+++ PowerShellEx.ps1
@@ -9,3 +9,15 @@ $total
 $n = 9
 $exp = $n % 2
 $exp
+$num1 = 10
+($num1 -le 10)
+
+$var1 = $False
+$var2 = $True
+($var1 -or $var2) -and $var2
+$var1 = "true"
+$var1 -eq $True
+$num1 = 20
+$num2 = 20%3
+($num2 -ne 2) -and $true
+
